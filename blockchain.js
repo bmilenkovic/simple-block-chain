f4c5903a281faf5f27f664903d8a0a62d6549c20
Branislav Milenkovic <branislav.milenkovic@gmail.com>
2021-06-02 08:53:27 -0500

    3.17 Chain Validation

--- blockchain.js
+++ blockchain.js
@@ -12,6 +12,27 @@ class Blockchain {
 
         return block;
     }
+
+    isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            console.log('Failed on Genesis check');
+            return false;
+        }
+        for (let i = 1; i < chain.length; i++ ) {
+            const block = chain[i];
+            const lastBlock = chain[i-1];
+            
+            if (block.lastHash !== lastBlock.hash) {
+                console.log('failed on prev block');
+                return false;
+            } else if (block.hash !== Block.blockHash(block)) {
+                console.log('failed on current block');
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
 
 module.exports = Blockchain;
