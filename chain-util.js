77a9205a6ad6721bfe115989063219124e1e2f90
Branislav Milenkovic <branislav.milenkovic@gmail.com>
2021-06-03 17:17:24 -0500

    7.37 Chain util and key generation

---
+++ chain-util.js
@@ -0,0 +1,19 @@
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+class ChainUtil {
+    static genKeyPair() {
+        return ec.genKeyPair();
+    }
+
+    static id () {
+        return uuidV1();
+    }
+
+    static hash(data) {
+        return SHA256(JSON.stringify(data).toString());
+    }
+}
+
+
+module.exports = ChainUtil;
--- dev-test.js
+++ dev-test.js
@@ -1,10 +1,3 @@
-const Blockchain = require('./blockchain');
-const Block = require('./blockchain');
-
-// const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
-// console.log(fooBlock.toString());
-
-const bc = new Blockchain();
-for (let i=0; i<10; i++) {
-    console.log(bc.addBlock(`foo ${i}`).toString());
-}
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString()); 
